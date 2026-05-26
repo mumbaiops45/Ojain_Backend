013c4fceaee8a7c1c6bd6141d9c61326a11cce9d
mumbaiops45 <mumbaiops45@gmail.com>
2026-05-26 12:27:52 +0530

    updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,31 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(
+//       process.env.MONGO_URI
+//     );
+
+//     console.log(
+//       `MongoDB Connected: ${conn.connection.host}`
+//     );
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+
+    // DEBUG
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
     const conn = await mongoose.connect(
       process.env.MONGO_URI
     );
@@ -9,6 +33,7 @@ const connectDB = async () => {
     console.log(
       `MongoDB Connected: ${conn.connection.host}`
     );
+
   } catch (error) {
     console.log(error.message);
     process.exit(1);
